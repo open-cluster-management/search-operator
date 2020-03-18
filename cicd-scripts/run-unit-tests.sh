6dddb0949a56bb9d44fc14f45fe5961906b6af0b
Marquis Hackett <Marquis Hackett>
2020-03-18 11:11:46 -0400

    make deps before test

--- cicd-scripts/run-unit-tests.sh
+++ cicd-scripts/run-unit-tests.sh
@@ -2,5 +2,7 @@
 set -e
 
 echo "> Running build/run-unit-tests.sh"
-
+go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
+go get -u github.com/golang/dep/cmd/dep
+dep ensure -v
 make unit-tests
