368aee79f42dd129a760e9d52941ebe9830775be
Sherin Varughese <shvarugh@redhat.com>
2020-11-10 16:14:14 -0500

    remove table tests

--- pkg/controller/searchservice/searchservice_controller_test.go
+++ pkg/controller/searchservice/searchservice_controller_test.go
@@ -6,22 +6,24 @@ import (
 	"context"
 	"reflect"
 	"testing"
-	"time"
 
+	errors "k8s.io/apimachinery/pkg/api/errors"
+
+	"github.com/open-cluster-management/search-operator/pkg/apis/search/v1alpha1"
 	searchv1alpha1 "github.com/open-cluster-management/search-operator/pkg/apis/search/v1alpha1"
+	assert "github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-func TestReconcileSearchService_Reconcile(t *testing.T) {
-	testscheme := scheme.Scheme
-	testscheme.AddKnownTypes(searchv1alpha1.SchemeGroupVersion, &searchv1alpha1.SearchService{})
+func commonSetup() (*runtime.Scheme, reconcile.Request, *v1alpha1.SearchService, *corev1.Secret) {
+	testScheme := scheme.Scheme
+	testScheme.AddKnownTypes(searchv1alpha1.SchemeGroupVersion, &searchv1alpha1.SearchService{})
 
 	testSearchService := &searchv1alpha1.SearchService{
 		TypeMeta: metav1.TypeMeta{
@@ -51,100 +53,82 @@ func TestReconcileSearchService_Reconcile(t *testing.T) {
 	// 		"redispwd": generatePass(16),
 	// 	},
 	// }
-
 	req := reconcile.Request{
 		NamespacedName: types.NamespacedName{
 			Name:      "test-cluster",
 			Namespace: "test-cluster",
 		},
 	}
+	return testScheme, req, testSearchService, testSecret
+}
 
-	type args struct {
-		request reconcile.Request
+func Test_searchServiceNotFound(t *testing.T) {
+	testScheme, req, _, _ := commonSetup()
+
+	client := fake.NewFakeClientWithScheme(testScheme)
+	nilSearchService := ReconcileSearchService{client, testScheme}
+
+	_, err := nilSearchService.Reconcile(req)
+	if !assert.Nil(t, err) {
+		t.Error("Expected Nil. Got error: ", err.Error())
 	}
-	type fields struct {
-		client client.Client
-		scheme *runtime.Scheme
+	instance := &searchv1alpha1.SearchService{}
+	err = client.Get(context.TODO(), req.NamespacedName, instance)
+	if !errors.IsNotFound(err) {
+		t.Error("Expected Not Found error. Got ", err.Error())
 	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    reconcile.Result
-		wantErr bool
-	}{
-		{
-			name: "search service not found",
-			fields: fields{
-				client: fake.NewFakeClientWithScheme(testscheme),
-				scheme: testscheme,
-			},
-			args: args{
-				request: req,
-			},
-			want: reconcile.Result{
-				Requeue:      false,
-				RequeueAfter: 0 * time.Second,
-			},
-			wantErr: false,
-		},
-		{
-			name: "secret not found", //check if secret got created
-			fields: fields{
-				client: fake.NewFakeClientWithScheme(testscheme, testSearchService),
-				scheme: testscheme,
-			},
-			args: args{
-				request: req,
-			},
-			want: reconcile.Result{
-				Requeue:      false,
-				RequeueAfter: 0 * time.Second,
-			},
-			wantErr: false,
-		},
-		{
-			name: "secret already exists", //check if existing secret is same as testSecret
-			fields: fields{
-				client: fake.NewFakeClientWithScheme(testscheme, testSearchService, testSecret),
-				scheme: testscheme,
-			},
-			args: args{
-				request: req,
-			},
-			want: reconcile.Result{
-				Requeue:      false,
-				RequeueAfter: 0 * time.Second,
-			},
-			wantErr: false,
-		},
+}
+
+func Test_secretNotFound(t *testing.T) {
+	testScheme, req, searchService, testSecret := commonSetup()
+
+	client := fake.NewFakeClientWithScheme(testScheme, searchService)
+	nilSearchService := ReconcileSearchService{client, testScheme}
+
+	_, err := nilSearchService.Reconcile(req)
+	if !assert.Nil(t, err) {
+		t.Error("Expected Nil. Got error: ", err.Error())
+	}
+	instance := &searchv1alpha1.SearchService{}
+	err = client.Get(context.TODO(), req.NamespacedName, instance)
+	if !assert.Nil(t, err) {
+		t.Error("Expected search Service. Got ", err.Error())
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			r := &ReconcileSearchService{
-				client: tt.fields.client,
-				scheme: tt.fields.scheme,
-			}
+	found := &corev1.Secret{}
+	err = client.Get(context.TODO(), types.NamespacedName{Name: testSecret.Name, Namespace: testSecret.Namespace}, found)
+	if err != nil {
+		t.Error("Expected secret to be created. Got error: ", err.Error())
+	}
+	if found.Name != testSecret.Name || found.Namespace != testSecret.Namespace || !reflect.DeepEqual(found.GetLabels(), testSecret.GetLabels()) {
+		t.Errorf("Found secret = %v/%v/%v, Expected =  %v/%v/%v", found.Name, found.Namespace, found.GetLabels(), testSecret.Name, testSecret.Namespace, testSecret.GetLabels())
+	}
 
-			got, err := r.Reconcile(tt.args.request)
+}
 
-			if (err != (error)(nil)) != tt.wantErr {
-				t.Errorf("ReconcileSearchService.Reconcile() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
+func Test_secretAlreadyExists(t *testing.T) {
+	testScheme, req, searchService, testSecret := commonSetup()
 
-			if tt.name == "secret not found" || tt.name == "secret already exists" {
-				found := &corev1.Secret{}
-				err = r.client.Get(context.TODO(), types.NamespacedName{Name: testSecret.Name, Namespace: testSecret.Namespace}, found)
+	client := fake.NewFakeClientWithScheme(testScheme, searchService, testSecret)
+	nilSearchService := ReconcileSearchService{client, testScheme}
 
-				if found.Name != testSecret.Name || found.Namespace != testSecret.Namespace || !reflect.DeepEqual(found.GetLabels(), testSecret.GetLabels()) {
-					t.Errorf("Secrets not the same = %v/%v/%v, want %v/%v/%v", found.Name, found.Namespace, found.GetLabels(), testSecret.Name, testSecret.Namespace, testSecret.GetLabels())
-				}
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("ReconcileSearchService.Reconcile() = %v, want %v", got, tt.want)
-			}
-		})
+	_, err := nilSearchService.Reconcile(req)
+	if !assert.Nil(t, err) {
+		t.Error("Expected Nil. Got error: ", err.Error())
+	}
+	instance := &searchv1alpha1.SearchService{}
+	err = client.Get(context.TODO(), req.NamespacedName, instance)
+	if !assert.Nil(t, err) {
+		t.Error("Expected search Service. Got ", err.Error())
+	}
+
+	found := &corev1.Secret{}
+	err = client.Get(context.TODO(), types.NamespacedName{Name: testSecret.Name, Namespace: testSecret.Namespace}, found)
+	if err != nil {
+		t.Error("Expected secret not found. Got error: ", err.Error())
+	}
+
+	if !reflect.DeepEqual(found.GetObjectMeta(), testSecret.GetObjectMeta()) || !reflect.DeepEqual(found.Data, testSecret.Data) {
+		t.Errorf("Expected secret not found. Secrets not the same - Check data part. Found secret = %v/%v/%v Data: %v, Expected =  %v/%v/%v Data: %v", found.Name, found.Namespace, found.GetLabels(), found.Data, testSecret.Name, testSecret.Namespace, testSecret.GetLabels(), testSecret.Data)
 	}
 }
