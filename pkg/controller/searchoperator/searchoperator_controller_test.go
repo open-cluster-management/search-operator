37e6ebd48b0316044817d908915f019d460ad13f
Sherin Varughese <shvarugh@redhat.com>
2020-11-13 06:39:01 -0500

    add deployment test

--- pkg/controller/searchoperator/searchoperator_controller_test.go
+++ pkg/controller/searchoperator/searchoperator_controller_test.go
@@ -10,7 +10,9 @@ import (
 	"github.com/open-cluster-management/search-operator/pkg/apis/search/v1alpha1"
 	searchv1alpha1 "github.com/open-cluster-management/search-operator/pkg/apis/search/v1alpha1"
 	"github.com/stretchr/testify/assert"
+	appv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -20,9 +22,10 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-func commonSetup() (*runtime.Scheme, reconcile.Request, *v1alpha1.SearchOperator, *corev1.Secret) {
+func commonSetup() (*runtime.Scheme, reconcile.Request, *v1alpha1.SearchOperator, *corev1.Secret, *appv1.Deployment) {
 	testScheme := scheme.Scheme
 	testScheme.AddKnownTypes(searchv1alpha1.SchemeGroupVersion, &searchv1alpha1.SearchOperator{})
+	testScheme.AddKnownTypes(corev1.SchemeGroupVersion, &corev1.Secret{})
 
 	testSearchOperator := &searchv1alpha1.SearchOperator{
 		TypeMeta: metav1.TypeMeta{
@@ -33,21 +36,27 @@ func commonSetup() (*runtime.Scheme, reconcile.Request, *v1alpha1.SearchOperator
 			Name:      "test-cluster",
 			Namespace: "test-cluster",
 		},
-		Spec: searchv1alpha1.SearchOperatorSpec{},
+		Spec: searchv1alpha1.SearchOperatorSpec{
+			Persistence: false,
+		},
 	}
 	testSecret := newRedisSecret(testSearchOperator)
-
 	req := reconcile.Request{
 		NamespacedName: types.NamespacedName{
 			Name:      "test-cluster",
 			Namespace: "test-cluster",
 		},
 	}
-	return testScheme, req, testSearchOperator, testSecret
+
+	emptyDirVolume := v1.VolumeSource{
+		EmptyDir: &v1.EmptyDirVolumeSource{},
+	}
+	deployment := getDeployment(testSearchOperator, emptyDirVolume)
+	return testScheme, req, testSearchOperator, testSecret, deployment
 }
 
 func Test_searchOperatorNotFound(t *testing.T) {
-	testScheme, req, _, _ := commonSetup()
+	testScheme, req, _, _, _ := commonSetup()
 
 	client := fake.NewFakeClientWithScheme(testScheme)
 	nilSearchOperator := ReconcileSearchOperator{client, testScheme}
@@ -64,7 +73,7 @@ func Test_searchOperatorNotFound(t *testing.T) {
 }
 
 func Test_secretCreatedWithOwnerRef(t *testing.T) {
-	testScheme, req, searchOperator, testSecret := commonSetup()
+	testScheme, req, searchOperator, testSecret, _ := commonSetup()
 
 	client := fake.NewFakeClientWithScheme(testScheme, searchOperator)
 	nilSearchOperator := ReconcileSearchOperator{client, testScheme}
@@ -99,7 +108,7 @@ func Test_secretCreatedWithOwnerRef(t *testing.T) {
 }
 
 func Test_secretAlreadyExists(t *testing.T) {
-	testScheme, req, searchOperator, testSecret := commonSetup()
+	testScheme, req, searchOperator, testSecret, _ := commonSetup()
 
 	client := fake.NewFakeClientWithScheme(testScheme, searchOperator, testSecret)
 	nilSearchOperator := ReconcileSearchOperator{client, testScheme}
@@ -123,4 +132,47 @@ func Test_secretAlreadyExists(t *testing.T) {
 	if !reflect.DeepEqual(found.GetObjectMeta(), testSecret.GetObjectMeta()) || !reflect.DeepEqual(found.Data, testSecret.Data) {
 		t.Errorf("Expected secret not found. Secrets not the same - Check data part. Found secret = %v/%v/%v Data: %v, Expected =  %v/%v/%v Data: %v", found.Name, found.Namespace, found.GetLabels(), found.Data, testSecret.Name, testSecret.Namespace, testSecret.GetLabels(), testSecret.Data)
 	}
+}
+
+func Test_DeploymentCreatedWithOwnerRef(t *testing.T) {
+	testScheme, req, searchOperator, testSecret, testDeployment := commonSetup()
+
+	//TODO: Passing already existing secret doesn't set ownerRef - testSecret
+	client := fake.NewFakeClientWithScheme(testScheme, searchOperator)
+	nilSearchOperator := ReconcileSearchOperator{client, testScheme}
+
+	_, err := nilSearchOperator.Reconcile(req)
+
+	instance := &searchv1alpha1.SearchOperator{}
+	err = client.Get(context.TODO(), req.NamespacedName, instance)
+	if !assert.Nil(t, err) {
+		t.Error("Expected search Operator. Got ", err.Error())
+	}
+
+	found := &corev1.Secret{}
+	err = client.Get(context.TODO(), types.NamespacedName{Name: testSecret.Name, Namespace: searchOperator.Namespace}, found)
+	if err != nil {
+		t.Error("Expected secret not found. Got error: ", err.Error())
+	}
+
+	foundDeployment := &appv1.Deployment{}
+	err = client.Get(context.TODO(), types.NamespacedName{Name: testDeployment.Name, Namespace: testDeployment.Namespace}, foundDeployment)
+	if err != nil {
+		t.Error("Expected deployment not found. Got error: ", err.Error())
+	}
+
+	if foundDeployment.Name != testDeployment.Name || foundDeployment.Namespace != testDeployment.Namespace || !reflect.DeepEqual(foundDeployment.Spec, testDeployment.Spec) {
+		t.Errorf("Expected deployment not found. Deployments not the same. Found deployment = %v/%v/%v Spec: %v, Expected =  %v/%v/%v Spec: %v", foundDeployment.Name, foundDeployment.Namespace, foundDeployment.GetLabels(), foundDeployment.Spec, testDeployment.Name, testDeployment.Namespace, testDeployment.GetLabels(), testDeployment.Spec)
+	}
+
+	ownerRefArray := foundDeployment.GetOwnerReferences()
+	if ownerRefArray == nil {
+		t.Error("Deployment does not have ownerReference set")
+	} else {
+		ownerRef := ownerRefArray[0]
+		if ownerRef.APIVersion != searchOperator.APIVersion || ownerRef.Kind != searchOperator.Kind || ownerRef.Name != searchOperator.Name {
+			t.Errorf("Deployment does not have correct ownerReference set. Owner should be searchOperator. Found %v/%v/%v. Expected %v/%v/%v", ownerRef.APIVersion, ownerRef.Kind, ownerRef.Name, searchOperator.APIVersion, searchOperator.Kind, searchOperator.Name)
+		}
+	}
+
 }
