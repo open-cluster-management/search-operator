6a9c169ac822676f33d3d46686d3fe5c6901b356
Sherin Varughese <shvarugh@redhat.com>
2020-12-22 23:59:38 -0500

    add ctrl

--- controllers/searchoperator_controller_test.go
+++ controllers/searchoperator_controller_test.go
@@ -16,6 +16,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
