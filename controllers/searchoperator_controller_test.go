844510a87beda20a150c440d46541c531faee0ca
Sherin Varughese <shvarugh@redhat.com>
2020-12-23 11:46:02 -0500

    Revert "add ctrl"
    This reverts commit 6a9c169ac822676f33d3d46686d3fe5c6901b356.

--- controllers/searchoperator_controller_test.go
+++ controllers/searchoperator_controller_test.go
@@ -16,7 +16,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
-	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
