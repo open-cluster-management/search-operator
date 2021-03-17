b663797290b60262eb867f769cac62a5c2415d21
Jorge Padilla <jpadilla@redhat.com>
2021-03-16 21:44:20 -0400

    Update run-unit-tests.sh

--- cicd-scripts/run-unit-tests.sh
+++ cicd-scripts/run-unit-tests.sh
@@ -4,7 +4,5 @@
 set -e
 
 echo "> Running build/run-unit-tests.sh"
-# GO111MODULE=on go get github.com/golangci/golangci-lint/cmd/golangci-lint@v1.33.0
-#go get -u github.com/golang/dep/cmd/dep
-#dep ensure -v
+
 make unit-tests
