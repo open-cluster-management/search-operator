db38ac832712e8db79bdf688fa862ad7f94654d4
xavier dharmaiyan <xavier@redhat.com>
2021-09-20 16:19:47 -0400

    added debug statement to get SHARED DIR
    Signed-off-by: xavier dharmaiyan <xavier@redhat.com>

--- cicd-scripts/run-e2e-tests.sh
+++ cicd-scripts/run-e2e-tests.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "SHARED_DIR=$SHARED_DIR"
+set -euxo pipefail
+
+
 KEY="$SHARED_DIR/private.pem"
 chmod 400 "$KEY"
 
 IP="$(cat "$SHARED_DIR/public_ip")"
 HOST="ec2-user@$IP"
 OPT=(-q -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i "$KEY")
+
 
 scp "${OPT[@]}" -r ../search-operator "$HOST:/tmp/search-operator"
 ssh "${OPT[@]}" "$HOST" /tmp/search-operator/cicd-scripts/run-e2e-tests.sh $COMPONENT_IMAGE_REFß 
